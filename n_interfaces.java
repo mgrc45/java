1721f2c43c0a4efd2814811452d9714a665022e0
Angel González <32023587+mgrc45@users.noreply.github.com>
2018-10-28 22:57:30 -0600

    Add files via upload

---
+++ n_interfaces.java
@@ -0,0 +1,21 @@
+/*
+ * Las interfaces es marcar una serie de elementos que tienes que implementar si o si. Por ello se le dice que es un contrato.
+ * 
+ * Una interface puede heredar de una o muchas interfaces
+ * Al implementar usamos la palabra reservada IMPLEMENTS
+ * Los atributos que coloques en una interface son estatico, publicos y finales.
+ * Los metodos son public, abstract.
+ * Las clases abstractas pueden definir atributos de instancia.
+ * 
+ * Diferencia: Las interfaces solo definen metodos estaticos y finales.
+ * Una interface te permite acceder a solo una parte de las propiedades.
+ *  --Lambda : Una funcion que pasas como parametro. Pasar como argumento la implementacion de un metodo.
+ * Operador unario
+ * Clases anonimas
+ */
+package curso;
+
+
+public class n_interfaces {
+    
+}
---
+++ z_errores.java
@@ -0,0 +1,14 @@
+package curso;
+
+public class z_errores {
+
+    public static void main(String[] args) {
+        //Variables
+        //int quantity = 5.5;
+        
+        //Operaciones
+        //double a = 4, b = 0;
+        //double c = a / b;
+    }
+    
+}
